b21056a527dcb2e61eb0af8d383d6b016532399a
RAIMUNDO JUNIO SANTOS <107777031+MundaoRJS@users.noreply.github.com>
2023-12-19 12:59:35 -0300

    initial commit

---
+++ EncapsulamentoStudys/src/estudandoencapsulamento/EstudandoMatriz.java
@@ -0,0 +1,30 @@
+package estudandoencapsulamento;
+
+//import java.util.Arrays;
+
+public class EstudandoMatriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		String mes[] = {"jan","fev","mar","abr","mai","jun","jul","ago","set","out","nov","dez"};
+		int tot[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+			for (int c =0; c<=12; c++) {
+				System.out.println("O mês de "+ mes[c]+" tem "+ tot[c]+" dias ao todo");
+		}*/
+			
+		/*double num[]= {3.5, 4,5.8, 7.1,1};
+		Arrays.sort(num);//deixando em ordem
+		for (double valor: num) {
+		System.out.print(valor+", ");
+		}
+		System.out.println("");
+		int pos = Arrays.binarySearch(num,3.5);
+		
+		System.out.print("Encontrei  o valor na posição "+ pos);*/
+		
+		
+		
+		
+	}
+}
