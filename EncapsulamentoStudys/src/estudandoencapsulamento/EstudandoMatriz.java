0a4bd2a45b69eb8555f81267abf786c088b6a60e
RAIMUNDO JUNIO SANTOS <107777031+MundaoRJS@users.noreply.github.com>
2024-01-07 19:43:46 -0300

    commit

--- EncapsulamentoStudys/src/estudandoencapsulamento/EstudandoMatriz.java
+++ EncapsulamentoStudys/src/estudandoencapsulamento/EstudandoMatriz.java
@@ -1,7 +1,9 @@
 package estudandoencapsulamento;
 
 //import java.util.Arrays;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 public class EstudandoMatriz {
 
 	public static void main(String[] args) {
@@ -24,7 +26,42 @@ public class EstudandoMatriz {
 		System.out.print("Encontrei  o valor na posição "+ pos);*/
 		
 		
+		List<String> names = new ArrayList<>();
+		names.add("Nome 4");
+		names.add("Nome 2");
+		names.add("Nome 1");
+		names.add("Nome 3");
+		/*
+		 * 
+		 * // buscando elemento específico pelo indice
+		for(int l = 0; l < names.size(); l++) {
+			System.out.println(names.get(l));
+		}
+		// buscando o indice apartir de um elememto
+		System.out.println(names.indexOf("Nome 2"));
 		
+		// verificando se a lista está vazia
+		System.out.println(names.isEmpty());
+		//Verificando se na lista contém o elemento passado
+		System.out.println(names.contains("Nome 4"));
+		// Verificando o tamanho da lista
+		System.out.println(names.size());
+		
+		// Limpar a lista(Remover todos os elementos)
+		names.clear();
+		System.out.println(names.isEmpty());
+		
+		//Ordenando lista 
+		Collections.sort(names);
+		for(String name : names) {
+			System.out.println(name);
+		}
+		 */
+		
+		User userA = new User("Raimundo" , "Santos" );
+		System.out.println(userA.outPut());
+		System.out.println(userA.outPut(false));
+		System.out.println(userA.toString());
 		
 	}
 }
